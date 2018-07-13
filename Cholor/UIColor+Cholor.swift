10776bd27c5380e755f0cce6e6886befc53e50e9
Roberto Hernández Tamayo <roberto.htamayo@gmail.com>
2018-07-13 17:04:16 -0500

    Añadir extensión para UIColor

---
+++ Cholor/UIColor+Cholor.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Cholor.swift
+//  Cholor
+//
+//  Created by Roberto Hernández Tamayo on 13/07/18.
+//  Copyright © 2018 Roberto Hernandez Tamayo. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let azulFondo = UIColor(named: "Azul Fondo")
+    static let azulDestello = UIColor(named: "Azul Destello")
+}
