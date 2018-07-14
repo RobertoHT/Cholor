d9b01a8f72a1b259efc98ab044a200c3fa748904
Roberto Hernández Tamayo <roberto.htamayo@gmail.com>
2018-07-14 18:16:48 -0500

    Crear enumeración ClaveUserDefaults

---
+++ Cholor/Controladores/User_defaults+Cholor.swift
@@ -0,0 +1,9 @@
+//  Cholor App by Roberto Hernández Tamayo on 09/07/18.
+//  Copyright © 2018 Roberto Hernandez Tamayo.
+
+import Foundation
+
+/// Claves para obtener el nivel y los puntos guardados en *UserDefaults*
+enum ClaveUserDefaults {
+    case nivel, puntos, material
+}
